f6d5b5639eecc9757875a8354fa8ed87ca08354d
nhaorand <haorand@foxmail.com>
2025-01-02 12:12:59 +0000

    Deploying to gh-pages from @ haoran-ding/haoran-ding.github.io@72f66860608788896238027bf636419ba55b5c16 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -361,16 +361,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-give-a-talk-at-database-group-university-of-washington-hosted-by-prof-dan-suciu",
+          title: 'Give a talk at Database Group, University of Washington (Hosted by Prof. Dan...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
